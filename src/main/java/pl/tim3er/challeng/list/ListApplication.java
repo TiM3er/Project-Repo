10de27b940d661105f55612b8548457598b0d163
Piotr Witowski <witowskipiotr11@gmail.com>
2018-08-04 16:11:01 +0200

    test commint

--- src/main/java/pl/tim3er/challeng/list/ListApplication.java
+++ src/main/java/pl/tim3er/challeng/list/ListApplication.java
@@ -5,24 +5,31 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
+import pl.tim3er.challeng.list.Dao.IChallenge;
+import pl.tim3er.challeng.list.Model.Project;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ListApplication implements CommandLineRunner {
-	@Autowired
-	JdbcTemplate jdbcTemplate;
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
-	public static void main(String[] args) {
-		SpringApplication.run(ListApplication.class, args);
-	}
+    @Autowired
+    IChallenge iChallenge;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListApplication.class, args);
+    }
 
 
-	@Override
-	public void run(String... args) throws Exception {
-//		List<Project> projects = projectList();
-//		for (Project project : projects) {
-//			System.out.println(project.toString());
-//		}
+    @Override
+    public void run(String... args) throws Exception {
+        List<Project> projects = iChallenge.projectList();
+        for (Project project : projects) {
+            System.out.println(project.toString());
+        }
 
-	}
+    }
 
 }
